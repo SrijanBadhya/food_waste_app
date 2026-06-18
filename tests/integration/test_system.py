bc086c2b31183e60688ac446b9de83a5722975f5
SrijanBadhya <srijanbadhya@gmail.com>
2026-06-17 20:23:51 -0400

    Formatted according to the latest black version

--- tests/integration/test_system.py
+++ tests/integration/test_system.py
@@ -6,7 +6,6 @@ from unittest.mock import MagicMock
 import time
 import streamlit as st
 
-
 # Append root directory to path safely for execution
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
